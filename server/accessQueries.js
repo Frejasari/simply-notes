8916d04c6a14251c869b07de62ddc0ec8f4d685f
Sari <freja.sender@gmail.com>
2018-07-13 21:55:26 +0200

    add query to check if user has rights to edit the notebook

--- server/accessQueries.js
+++ server/accessQueries.js
@@ -4,6 +4,9 @@ module.exports = {
   findOneNotebookWithAccessThroughId: (notebookId, userId) =>
     Notebook.findOne({ $and: [{ _id: notebookId }, { _collaborators: userId }] }),
 
+  findOneNotebookWithAccessThroughIdAndUpdate: (notebookId, userId, updatedUser) =>
+    Notebook.findOneAndUpdate({ $and: [{ _owner: userId }, { _id: notebookId }] }, updatedUser, { new: true }),
+
   findOneNotebookWithAccessThroughSiteId: (siteId, userId) =>
     Notebook.findOne({ $and: [{ _sites: siteId }, { _collaborators: userId }] })
 };
