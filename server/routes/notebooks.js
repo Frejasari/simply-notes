9fec02155d8953327ed11ee87176c9504b7481f3
Sari <freja.sender@gmail.com>
2018-07-13 17:26:40 +0200

    add routes to get and create a notebook / all notebooks
    + Validation through postman done

--- server/routes/notebooks.js
+++ server/routes/notebooks.js
@@ -1,30 +1,32 @@
 var express = require("express");
 const Notebook = require("../models/Notebook");
+const Site = require("../models/Site");
 const passport = require("passport");
 const config = require("../config");
 
 var router = express.Router();
 
+//#region GET Notebooks // GET Notebooks/:NotebookId
 // Route to get all notebooks of the user
 router.get("/", passport.authenticate("jwt", config.jwtSession), (req, res, next) => {
-  // TODO : get the user from local variable to access the current user! and find all notebooks of this user.
-  console.log("ROUTER GET NOTEBOOKS CALLED, REWORK!!!! Only access notebooks of the current user!!");
-  Notebook.find()
+  console.log("ROUTER GET NOTEBOOKS CALLED");
+  Notebook.find({ _collaborators: req.user._id })
     .then(notebooks => {
       res.json(notebooks);
     })
     .catch(err => next(err));
 });
 
+// Route to get a specific notebook
 router.get("/:notebookId", passport.authenticate("jwt", config.jwtSession), (req, res, next) => {
-  console.log("ROUTER GET NOTEBOOKID CALLED");
-  // TODO: check that the notebook is accessible to the user !! (so she is the owner / collaborator )
-  const user = req.user;
-  Notebook.findById(req.params.notebookId)
+  Notebook.findOne({ $and: [{ _id: req.params.notebookId }, { _collaborators: req.user._id }] })
+    .populate("_sites")
     .then(notebook => {
-      if (notebook._collaborators.includes(user._id)) {
+      if (notebook) {
         res.json(notebook);
-      } else res.redirect("/error");
+      } else res.json("ERROR");
+      // TODO REDIRECT TO SOME PAGE!!!
+      // res.redirect("/error");
     })
     .catch(err => next(err));
 });
@@ -32,12 +34,13 @@ router.get("/:notebookId", passport.authenticate("jwt", config.jwtSession), (req
 router.get("/error", (req, res, next) => {
   res.json(error);
 });
+//#endregion
 
-// Route to add a notebook
-router.post("/", (req, res, next) => {
-  let { title, description } = req.body;
-  // TODO: push curr user in
-  Notebook.create({ title, description })
+//#region POST Notebooks
+router.post("/", passport.authenticate("jwt", config.jwtSession), (req, res, next) => {
+  const { title, description } = req.body;
+  const userId = req.user._id;
+  Notebook.create({ title, description, _owner: userId, _collaborators: [userId] })
     .then(notebook => {
       res.json({
         success: true,
@@ -46,5 +49,6 @@ router.post("/", (req, res, next) => {
     })
     .catch(err => next(err));
 });
+//#endregion
 
 module.exports = router;
