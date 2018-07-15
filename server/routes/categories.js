a1e2436e8e1d09fc3697fe9b6a384f46db2babe1
Sari <freja.sender@gmail.com>
2018-07-15 16:53:13 +0200

    fix typo

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,5 +1,5 @@
 var express = require("express");
-const Categorie = require("../models/Category");
+const Category = require("../models/Category");
 const passport = require("passport");
 const config = require("../config");
 const accessQueries = require("../accessQueries");
