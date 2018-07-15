10a672da54157823f7501faf0036af94661507a4
Sari <freja.sender@gmail.com>
2018-07-15 16:53:56 +0200

    create route GET categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -6,4 +6,15 @@ const accessQueries = require("../accessQueries");
 
 var router = express.Router();
 
+//#region GET categories
+router.get("/", passport.authenticate("jwt", config.jwtSession), (req, res, next) => {
+  Category.find({ _owner: req.user._id })
+    .then(categories => {
+      if (!categories) next("Error, no categories found");
+      else res.json(categories);
+    })
+    .catch(err => next(err));
+});
+//#endregion
+
 module.exports = router;
