c56868d2a4004c4d17357ebe79414c8a95f73286
Sari <freja.sender@gmail.com>
2018-07-16 12:10:18 +0200

    create basic bootstrap navbar + create NavItem Component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+const NavItem = props => (
+  <li className="nav-item">
+    <NavLink to={props.path} exact className="nav-link">
+      {props.children}
+    </NavLink>
+  </li>
+);
+
+class NavDropdown extends Component {}
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm fixed-top navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <NavItem path="/">Home</NavItem>
+            <NavItem path="/notebooks">Notebooks</NavItem>
+            {/* <NavItem path="/ /">Home</NavItem> */}
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider" />
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
