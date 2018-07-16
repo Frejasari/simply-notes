1acf1f73197910dbcb5347e5dd35ee9ec4770cf1
Sari <freja.sender@gmail.com>
2018-07-16 12:08:29 +0200

    import bootstrap 4 for styling

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
