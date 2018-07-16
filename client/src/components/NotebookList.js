5397f5600440c948396c15723e4cd0140ef06a84
Sari <freja.sender@gmail.com>
2018-07-16 14:51:37 +0200

    update notebookListItems to show description and modularize code

--- client/src/components/NotebookList.js
+++ client/src/components/NotebookList.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import api from "../api";
 
+const NotebookListItem = props => {
+  console.log("PROPS", props);
+  return (
+    <li>
+      <Link to={`/notebooks/${props.notebook._id}`}>{props.notebook.title}</Link>
+      <p>{props.notebook.description}</p>
+    </li>
+  );
+};
+
 class NotebookList extends Component {
   constructor(props) {
     super(props);
@@ -23,11 +33,7 @@ class NotebookList extends Component {
     return (
       <div className="Notebooks">
         <h2>List of notebooks</h2>
-        {this.state.notebooks.map(notebook => (
-          <li key={notebook._id}>
-            <Link to={`/notebooks/${notebook._id}`}>{notebook.title}</Link>
-          </li>
-        ))}
+        {this.state.notebooks.map(notebook => <NotebookListItem notebook={notebook} key={notebook._id} />)}
       </div>
     );
   }
