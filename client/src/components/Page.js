2bf305d301d6c4a1debc42313402e4e48cd0b1be
Sari <freja.sender@gmail.com>
2018-07-17 12:12:39 +0200

    use Component with ContentEditableDiv for Paragraph to make text input smooth

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -1,90 +1,6 @@
 import React, { Component } from "react";
 import api from "../api";
-import "./Page.css";
-
-const DEFAULT_HEIGHT = 20;
-
-class Paragraph extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      height: DEFAULT_HEIGHT,
-      text: this.props.text
-    };
-    this.mounted = false;
-    this.ghost = null;
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.setFilledTextareaHeight = this.setFilledTextareaHeight.bind(this);
-  }
-
-  setFilledTextareaHeight() {
-    console.log("----- KU HEIGHT");
-    if (this.mounted) {
-      const newHeight = this.ghost.clientHeight + DEFAULT_HEIGHT;
-      if (newHeight !== this.state.height)
-        this.setState({
-          height: newHeight
-        });
-    }
-  }
-
-  handleTextChange(event) {
-    console.log("----- HVC HEIGHT");
-    const text = event.target.value;
-    if (this.mounted) {
-      this.setState({ text: text });
-    }
-  }
-
-  getExpandableField() {
-    // console.log("GET EXPANDABLE FIELD");
-    const { height, text } = this.state;
-    return (
-      <textarea
-        className="textarea"
-        name="textarea"
-        id="textarea"
-        autoFocus={true}
-        defaultValue={text}
-        style={{
-          height
-        }}
-        onChange={this.handleTextChange}
-        onKeyUp={this.setFilledTextareaHeight}
-      />
-    );
-  }
-
-  getGhostField() {
-    // console.log("GET GHOST FIELD");
-    return (
-      <div className="textarea textarea--ghost" ref={c => (this.ghost = c)} aria-hidden="true">
-        {this.state.text}
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    console.log("DID MOUNT");
-    this.mounted = true;
-    this.setFilledTextareaHeight();
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  render() {
-    console.log("RENDER");
-
-    return (
-      <div className="paragraph-container">
-        {this.getGhostField()}
-        {this.getExpandableField()}
-      </div>
-    );
-  }
-}
+import Paragraph from "./Paragraph";
 
 class Page extends Component {
   constructor(props) {
@@ -115,9 +31,9 @@ class Page extends Component {
       <div className="Notebooks">
         <h2>{page.title}</h2>
         <p>{page.description}</p>
-        {page._paragraphs.map(p => (
+        {page._paragraphs.map((p, i) => (
           <div key={p._id}>
-            <Paragraph text={p.text} />
+            <Paragraph paragraph={p} index={i} />
           </div>
         ))}
       </div>
