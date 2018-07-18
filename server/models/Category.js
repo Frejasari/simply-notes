d03fa31041e863a5beb6f9902b57c11f407e6697
Sari <freja.sender@gmail.com>
2018-07-18 18:50:00 +0200

    add color to category model so the user can choose a color

--- server/models/Category.js
+++ server/models/Category.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const categorySchema = new Schema(
   {
     name: { type: String, required: [true, "A category name is required"] },
+    color: { type: String, default: "#8FA69C" },
     _owner: { type: Schema.Types.ObjectId, ref: "User", required: "An owner is required" }
   },
   { timestamps: { createdAt: "created_at", updatedAt: "updatedAt" } }
