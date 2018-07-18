404bc05aa637ca3322bad5593bc1e442e1c2a97e
Sari <freja.sender@gmail.com>
2018-07-18 18:49:04 +0200

    import reactstrap for bootstrap implementation

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
 // import registerServiceWorker from './registerServiceWorker';
