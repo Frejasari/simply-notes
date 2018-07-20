0beb4b5d47d33f3bc2bca6ec0ad22bc4abe79c70
Sari <freja.sender@gmail.com>
2018-07-20 08:54:23 +0200

    remove placeholder

--- client/src/components/EditAndCreateForm.js
+++ client/src/components/EditAndCreateForm.js
@@ -2,17 +2,23 @@ import React from "react";
 import { Col, Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 export default class EditAndCreateForm extends React.Component {
-  
   render() {
     return (
       <Form>
         <FormGroup>
           <Label for="title">Title</Label>
-          <Input type="text" name="title" id="title" placeholder="Enter title" value={this.props.title} onChange={this.props.handleTitleChange}/>
+          <Input type="text" name="title" id="title" value={this.props.title} onChange={this.props.handleTitleChange} />
         </FormGroup>
         <FormGroup>
           <Label for="description">Description</Label>
-          <Input type="textarea" name="description" id="description" style={{ resize: "none" }} value={this.props.description} onChange={this.props.handleDescriptionChange}/>
+          <Input
+            type="textarea"
+            name="description"
+            id="description"
+            style={{ resize: "none" }}
+            value={this.props.description}
+            onChange={this.props.handleDescriptionChange}
+          />
         </FormGroup>
       </Form>
     );
